727d6661a6f7fd3b35e9b2d3b9e5ebdd9dc7fbb9
py1u.dev <py1u.dev@gmail.com>
2024-10-02 22:24:32 -0700

    test: flask app with routes

---
+++ flask/server.py
@@ -0,0 +1,29 @@
+from flask import Flask,jsonify
+from datetime import date
+
+app = Flask(__name__)
+
+about_developer = {
+    'name': 'peter',
+    'role': 'software engineer',
+}
+
+about_project = {
+    'name': 'eigress',
+    'start': date.fromisoformat('2024-09-17')
+}
+
+@app.route("/flask/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/flask/project")
+def get_data():
+    return jsonify(about_project)
+
+@app.route("/flask/dev")
+def get_developer():
+    return jsonify(about_developer)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
