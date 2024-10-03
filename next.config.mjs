123503cf2202347bdf6493588bc3a9090ef133d3
py1u.dev <py1u.dev@gmail.com>
2024-10-02 22:23:59 -0700

    config: flask api integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: "/flask/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5328/flask/:path*"
+            : "/flask/",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
